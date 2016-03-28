dfbad703bcf92f85d3f0204973fa8e0becd5421e
kiphe19 <kiphe19@gmail.com>
2016-03-28 19:22:25 +0700

    Initial Commit

---
+++ application/views/inc/_content.php
@@ -0,0 +1,147 @@
+	<div class="ui grid fluid computer large monitor desktop tablet only padded divided" style="padding-top:3rem;">
+		<div class="three wide column">
+			<div class="ui vertical pointing menu fixed" style="top: 4rem;left: 1rem;">
+				<a class="active item" href="/">Home</a>
+				<a class="item" href="#">Messages</a>
+				<div class="ui dropdown item" href="#">
+					Friends
+					<i class="dropdown icon"></i>
+					<div class="left menu">
+						<a class="item"><i class="edit icon"></i> Edit Profile</a>
+						<a class="item"><i class="globe icon"></i> Choose Language</a>
+						<a class="item"><i class="settings icon"></i> Account Settings</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="eleven wide column">
+			<div class="ui tall stacked segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="image">
+							<!-- <img src="/images/wireframe/image.png"> -->
+						</div>
+						<div class="content">
+							<a class="header">Header</a>
+							<div class="meta">
+								<span>Description</span>
+							</div>
+							<div class="description">
+								<p></p>
+							</div>
+							<div class="extra">
+								Additional Details
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ui tall stacked segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="image">
+							<!-- <img src="/images/wireframe/image.png"> -->
+						</div>
+						<div class="content">
+							<a class="header">Header</a>
+							<div class="meta">
+								<span>Description</span>
+							</div>
+							<div class="description">
+								<p></p>
+							</div>
+							<div class="extra">
+								Additional Details
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ui tall stacked segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="image">
+							<!-- <img src="/images/wireframe/image.png"> -->
+						</div>
+						<div class="content">
+							<a class="header">Header</a>
+							<div class="meta">
+								<span>Description</span>
+							</div>
+							<div class="description">
+								<p></p>
+							</div>
+							<div class="extra">
+								Additional Details
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ui tall stacked segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="image">
+							<!-- <img src="/images/wireframe/image.png"> -->
+						</div>
+						<div class="content">
+							<a class="header">Header</a>
+							<div class="meta">
+								<span>Description</span>
+							</div>
+							<div class="description">
+								<p></p>
+							</div>
+							<div class="extra">
+								Additional Details
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ui tall stacked segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="image">
+							<!-- <img src="/images/wireframe/image.png"> -->
+						</div>
+						<div class="content">
+							<a class="header">Header</a>
+							<div class="meta">
+								<span>Description</span>
+							</div>
+							<div class="description">
+								<p></p>
+							</div>
+							<div class="extra">
+								Additional Details
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ui tall stacked segment">
+				<div class="ui items">
+					<div class="item">
+						<div class="image">
+							<!-- <img src="/images/wireframe/image.png"> -->
+						</div>
+						<div class="content">
+							<a class="header">Header</a>
+							<div class="meta">
+								<span>Description</span>
+							</div>
+							<div class="description">
+								<p></p>
+							</div>
+							<div class="extra">
+								Additional Details
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<script type="text/javascript" src="{base_url}public/js/main.js"></script>
