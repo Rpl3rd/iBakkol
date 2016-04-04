4e52639bbf721ceb878cf967c8f36fba9e8ff28f
kiphe19 <kiphe19@gmail.com>
2016-04-04 09:39:20 +0700

    form validation, testing

--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,10 @@
 	$('.ui.dropdown').dropdown();
 	$('select').dropdown();
 	$('.menu .item').tab();
+	$('.ui.form.dt-tamatan').form({
+		fields: {
+			nisn: ['number', 'minLength[10]', 'maxLength[10]']
+		}
+	});
 
 })(window)
