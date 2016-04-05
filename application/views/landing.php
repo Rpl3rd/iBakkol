fc32d709d45309d2de66f8d478271a8904b45e57
kiphe19 <kiphe19@gmail.com>
2016-04-05 19:12:19 +0700

    Editing almost, yeah

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,135 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
-  <title>Homepage</title>
+  <title>iBakkol</title>
   <link rel="stylesheet" type="text/css" href="{base_url}public/semantic.min.css">
-  <style type="text/css">
-
-    .hidden.menu {
-      display: none;
-    }
-
-    .masthead.segment {
-      min-height: 700px;
-      padding: 1em 0em;
-    }
-    .masthead .logo.item img {
-      margin-right: 1em;
-    }
-    .masthead .ui.menu .ui.button {
-      margin-left: 0.5em;
-    }
-    .masthead h1.ui.header {
-      margin-top: 3em;
-      margin-bottom: 0em;
-      font-size: 4em;
-      font-weight: normal;
-    }
-    .masthead h2 {
-      font-size: 1.7em;
-      font-weight: normal;
-    }
-
-    .ui.vertical.stripe {
-      padding: 8em 0em;
-    }
-    .ui.vertical.stripe h3 {
-      font-size: 2em;
-    }
-    .ui.vertical.stripe .button + h3,
-    .ui.vertical.stripe p + h3 {
-      margin-top: 3em;
-    }
-    .ui.vertical.stripe .floated.image {
-      clear: both;
-    }
-    .ui.vertical.stripe p {
-      font-size: 1.33em;
-    }
-    .ui.vertical.stripe .horizontal.divider {
-      margin: 3em 0em;
-    }
-
-    .quote.stripe.segment {
-      padding: 0em;
-    }
-    .quote.stripe.segment .grid .column {
-      padding-top: 5em;
-      padding-bottom: 5em;
-    }
-
-    .footer.segment {
-      padding: 5em 0em;
-    }
-
-    .secondary.pointing.menu .toc.item {
-      display: none;
-    }
-
-    @media only screen and (max-width: 700px) {
-      .ui.fixed.menu {
-        display: none !important;
-      }
-      .secondary.pointing.menu .item,
-      .secondary.pointing.menu .menu {
-        display: none;
-      }
-      .secondary.pointing.menu .toc.item {
-        display: block;
-      }
-      .masthead.segment {
-        min-height: 350px;
-      }
-      .masthead h1.ui.header {
-        font-size: 2em;
-        margin-top: 1.5em;
-      }
-      .masthead h2 {
-        margin-top: 0.5em;
-        font-size: 1.5em;
-      }
-    }
-
-
-  </style>
-
+  <link rel="stylesheet" type="text/css" href="{base_url}public/css/home.css">
   <script src="{base_url}public/plugin/jquery.min.js"></script>
   <script src="{base_url}public/semantic.min.js"></script>
-
-  <script>
-    $(document)
-    .ready(function() {
-
-      // fix menu when passed
-      $('.masthead')
-      .visibility({
-        once: false,
-        onBottomPassed: function() {
-          $('.fixed.menu').transition('fade in');
-        },
-        onBottomPassedReverse: function() {
-          $('.fixed.menu').transition('fade out');
-        }
-      })
-      ;
-
-      // create sidebar and attach to menu open
-      $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-      ;
-
-    })
-    ;
-  </script>
 </head>
 <body>
-
-  <!-- Following Menu -->
   <div class="ui large top fixed hidden menu">
     <div class="ui container">
       <a class="active item">Home</a>
@@ -148,7 +30,7 @@
   </div>
 
   <!-- Sidebar Menu -->
-  <div class="ui vertical inverted sidebar menu">
+  <div class="ui vertical sidebar menu">
     <a class="active item">Home</a>
     <a class="item">Work</a>
     <a class="item">Company</a>
@@ -156,14 +38,12 @@
     <a class="item">Login</a>
     <a class="item">Signup</a>
   </div>
-
-
   <!-- Page Contents -->
   <div class="pusher">
-    <div class="ui inverted vertical masthead center aligned segment">
+    <div class="ui inverted bg-image vertical masthead center aligned segment">
 
       <div class="ui container">
-        <div class="ui large secondary inverted pointing menu">
+        <div class="ui large secondary pointing menu inverted head-main">
           <a class="toc item">
             <i class="sidebar icon"></i>
           </a>
@@ -198,7 +78,7 @@
             <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.</p>
           </div>
           <div class="six wide right floated column">
-            <img src="assets/images/wireframe/white-image.png" class="ui large bordered rounded image">
+            <img src="{base_url}public/image/image.png" class="ui large bordered rounded image">
           </div>
         </div>
         <div class="row">
@@ -208,7 +88,6 @@
         </div>
       </div>
     </div>
-
 
     <div class="ui vertical stripe quote segment">
       <div class="ui equal width stackable internally celled grid">
@@ -220,7 +99,7 @@
           <div class="column">
             <h3>"I shouldn't have gone with their competitor."</h3>
             <p>
-              <img src="assets/images/avatar/nan.jpg" class="ui avatar image"> <b>Nan</b> Chief Fun Officer Acme Toys
+              <img src="{base_url}public/image/image.png" class="ui avatar image"> <b>Nan</b> Chief Fun Officer Acme Toys
             </p>
           </div>
         </div>
@@ -240,7 +119,6 @@
         <a class="ui large button">I'm Still Quite Interested</a>
       </div>
     </div>
-
 
     <div class="ui inverted vertical footer segment">
       <div class="ui container">
@@ -271,6 +149,6 @@
       </div>
     </div>
   </div>
-
+  <script src="{base_url}public/js/home.js"></script>
 </body>
 </html>
